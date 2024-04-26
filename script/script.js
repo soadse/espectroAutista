f998663a763969149ba13d3a25387f314711fec7
Rodrigo Andrade <144704436+soadse@users.noreply.github.com>
2024-04-26 16:42:30 -0300

    ....

--- script/script.js
+++ script/script.js
@@ -1,15 +1,65 @@
 var menu = window.document.querySelector('#menu')
 var menuIndex = window.document.querySelector('#menuIndex')
+var opc1 = document.querySelector('#opc1')
+var opc2 = document.querySelector('#opc2')
+var opc3 = document.querySelector('#opc3')
+var opc4 = document.querySelector('#opc4')
+var clos = window.document.querySelector('#closed')
 
-
-
-function showMenu(){
-    menu.style.display = 'flex'
+function mostra1(){
+    opc1.style.display = 'block'
+}
+function mostra2(){
+    opc2.style.display = 'block'
+}
+function mostra3(){
+    opc3.style.display = 'block'
+}
+function mostra4(){
+    opc4.style.display = 'block'
+}
+function mostra5(){
+    clos.style.display = 'block'
+}
+function saiMenu(){
     menuIndex.style.display = 'none'
+    menuIndex.style.opacity = '0%'
+}
+function showMenu(){
+    setTimeout(saiMenu, 110)
+    setTimeout(mostra1, 100)
+    setTimeout(mostra2, 150)
+    setTimeout(mostra3, 200)
+    setTimeout(mostra4, 250)
+    setTimeout(mostra5, 300)
 }
 
-function closeMenu(){
-    menu.style.display = 'none'
+
+function fecha1(){
+    opc1.style.display = 'none'
+}
+function fecha2(){
+    opc2.style.display = 'none'
+}
+function fecha3(){
+    opc3.style.display = 'none'
+}
+function fecha4(){
+    opc4.style.display = 'none'
+}
+function fecha5(){
+    clos.style.display = 'none'
+}
+function voltaMenu(){
     menuIndex.style.display = 'block'
+    menuIndex.style.opacity = '100%'
+}
+function closeMenu(){
+    setTimeout(voltaMenu, 300)
+    setTimeout(fecha1, 300)
+    setTimeout(fecha2, 250)
+    setTimeout(fecha3, 200)
+    setTimeout(fecha4, 150)
+    setTimeout(fecha5, 100)
 }
 
